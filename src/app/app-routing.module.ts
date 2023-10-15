489df6c8b99763276068f3d89483d6a5144c7ec4
luis.diaz.8belts <luis.diaz@8belts.com>
2023-10-15 14:00:28 +0200

    new option added to menu and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MatchFormComponent } from './components/match-form/match-form.component';
+import { MatchTableComponent } from './components/match-table/match-table.component';
 import { UserTableComponent } from './components/user-table/user-table.component';
 
 const routes: Routes = [
   { path: 'user', component: UserTableComponent },
-  { path: 'match', component: MatchFormComponent },
-  { path: '', redirectTo: '/table', pathMatch: 'full' }
+  { path: 'match', component: MatchTableComponent },
+  { path: 'insert-match', component: MatchFormComponent },
+  { path: '', redirectTo: '/user', pathMatch: 'full' }
 ];
 
 @NgModule({
